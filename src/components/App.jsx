8f05cd3989391a412ef1b50ffd04185c39ee5edc
Tetiana Nivchyk <tata.02od@gmail.com>
2023-04-28 14:14:56 +0300

    added first changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,43 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback() {}
+
+  countPositiveFeedbackPercentage() {}
+  render() {
+    return (
+      <section>
+        <strong>Please leave feedback</strong>
+        <ul>
+          <li>
+            <button type="button">Good</button>
+          </li>
+          <li>
+            <button type="button">Neutral</button>
+          </li>
+          <li>
+            <button type="button">Bad</button>
+          </li>
+        </ul>
+        <h1>Statistics</h1>
+        <ul>
+          <li>
+            Good:<span></span>
+          </li>
+          <li>
+            Neutral:<span></span>
+          </li>
+          <li>
+            Bad:<span></span>
+          </li>
+        </ul>
+      </section>
+    );
+  }
+}
